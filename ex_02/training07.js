758adab39876bb63dbf9fc17747a612f4e67ceb6
Stefan Sobek <s.sobek@fontys.nl>
2021-01-19 14:08:39 +0100

    final

---
+++ ex_02/training07.js
@@ -0,0 +1,24 @@
+// final Training Session
+
+// TODO
+
+// you need to have docker installed for this one
+// We will run a local database with a rest-api holding all the president data
+// Then we fetch data from the rest-api
+
+// 1. open a new terminal / cmd / console
+// 2. clone somewhere the following url:
+// 3. git clone https://github.com/FontysVenlo/president-postgrest-restapi.git
+// 4. go into the directory: cd president-postgrest-restapi
+// 5. in the terminal type: docker-compose -d
+// 6. type npx babel-node ex_02/training07.js
+
+const fetch = require('node-fetch');
+
+const getPresidents = () =>
+    fetch("http://localhost:3000/president")
+        .then(res => res.json())
+        .then(json => json.map(president => president.name))
+        .then(names => console.log(names));
+
+getPresidents();
