0599765e61c01019d05ecc3a0d9812d6ac37e82b
Stefan Sobek <s.sobek@fontys.nl>
2021-02-02 13:14:51 +0100

    Update training01.js

--- ex_01/training01.js
+++ ex_01/training01.js
@@ -1,11 +1,14 @@
 // Constants
+const salary = 1000;
+
+// uncomment the following line
+
+//salary = 100000;
 
 // Just run the code
 // Is it working? What is the output and why?
 // Fix it!!! Hint: make a new constant.
-const salary = 1000;
 
-salary = 100000;
 
 
 
