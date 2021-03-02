91ef720aedfd68df0695ff222e45cbc2edd8b8dc
Zali-Masodi <60880359+Zali-Masodi@users.noreply.github.com>
2021-03-02 18:14:14 +0100

    Update training01.js

--- ex_01/training01.js
+++ ex_01/training01.js
@@ -3,8 +3,10 @@ const salary = 1000;
 
 // uncomment the following line
 
-salary = 100000;
+const salary2 = 100000;
 
+console.log(salary);
+console.log(salary2);
 // Just run the code
 // Is it working? What is the output and why?
 // Fix it!!! Hint: make a new constant.
